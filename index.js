d6481df5a282f561838c93358c8f338c172b10df
lucasviola <lucasmatzenbacher@gmail.com>
2016-03-04 10:28:13 -0300

    Adds mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
 // Models
 var Movie = require('./app/models/movie');
