ac450e346197edfc4af860470b441e6a30d3848c
lucasviola <lucasmatzenbacher@gmail.com>
2016-03-04 09:37:09 -0300

    no changes at all

--- restful_spec.sh
+++ restful_spec.sh
@@ -2,6 +2,6 @@
 
 # Using DASH since we're not coding so much here and it is the most common shell
 
-# if test -z "which curl"; then
+# if [ -z "which curl" ] ; then
   curl http://localhost:3000/api # GET
 # fi
