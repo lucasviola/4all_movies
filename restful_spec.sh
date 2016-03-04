77b92eded18010d9e7e0d6924f2b9eab5235ae3d
lucasviola <lucasmatzenbacher@gmail.com>
2016-03-04 09:45:20 -0300

    Adds API spec

--- restful_spec.sh
+++ restful_spec.sh
@@ -2,6 +2,15 @@
 
 # Using DASH since we're not coding so much here and it is the most common shell
 
+# API Specification
+#  Route             |  Verb     |      Description
+# /api/<model>      |   GET     |      Get all the <models>.
+# /api/<model>     |   POST    |      Create a model.
+# /api/<model>/:id | GET | Get a single model
+# /api/<model>/:id | PUT | Update model
+# api/<model>/:id | DELETE | Delete model
+
+
 # if [ -z "which curl" ] ; then
   curl http://localhost:3000/api # GET
 # fi
